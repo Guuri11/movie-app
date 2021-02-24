1979a5c931da1ed55eaa5301fbd4f6485457bdc3
Guuri11 <sergio.gurillo11@gmail.com>
2021-02-24 20:55:44 +0100

    added navigation packages

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Text, SafeAreaView } from 'react-native'
 import { Provider } from 'react-native-paper'
-import { Card, Button } from 'react-native-paper';
-
+import { Button } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <Provider>
-      <SafeAreaView>
-        <Text>Hola mundo</Text>
-        <Button icon="image" mode="contained" onPress={()=>console.log('hola')} >Boton</Button>
-      </SafeAreaView>
+      <NavigationContainer>
+        <SafeAreaView>
+          <Text>Hola mundo</Text>
+          <Button icon="image" mode="contained" onPress={() => console.log('hola')} >Boton</Button>
+        </SafeAreaView>
+      </NavigationContainer>
     </Provider>
   )
 }

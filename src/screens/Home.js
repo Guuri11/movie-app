9d983c2a5129450fd092d0919a01c58002c5827d
Guuri11 <sergio.gurillo11@gmail.com>
2021-04-01 08:54:53 +0200

    first api request fetched

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
+import { getNewsMoviesApi } from '../api/movie'
 
 export default function Home() {
+
+    const [new_movies, setNewMovies] = useState(null)
+
+    console.log(new_movies);
+    useEffect(() => {
+        getNewsMoviesApi().then(response => setNewMovies(response.results))
+    }, [])
+
     return (
         <View>
             <Text>Estamos en home</Text>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
+export const API_HOST = "https://api.themoviedb.org/3"
 export const API_KEY = "74580ba8c079f11f5036a3203e65118b"
+export const LANG = "es-ES"
